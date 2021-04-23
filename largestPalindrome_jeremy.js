992ad64aa08bb4202e352582536ae5a5016c22d9
Jeremy Friedrichsen <jeremy.friedrichsen@gmail.com>
2021-04-23 09:58:05 -0600

    optimized

--- largestPalindrome_jeremy.js
+++ largestPalindrome_jeremy.js
@@ -6,7 +6,7 @@
 // Directions:
 //  1. Rename this file, substituting "yourname" with your name
 //  2. Replace "yourname" with your name in the variable below
-exports.name = "yourname";
+exports.name = "jeremy";
 //  3. Add optimizations to the solution below
 //  4. Upload your optimized solution 
 
@@ -15,41 +15,48 @@ isPalindrome = (num) => {
     // determine if input value matches it's reversed value (i.e. check if it's a palindrome!)
     strNum = num.toString();
     return strNum === strNum.split("").reverse().join("");
+    // let nums = []
+    // while( num > 0){
+    //     nums.push(num % 10)
+    //     num = Math.floor(num/10)
+    // }
+    // while( nums.length > 1){
+    //     if (nums.pop() !== nums.unshift()){
+    //         return false
+    //     }
+    //     return true
+    // }
 }
 
 
 exports.getLargestPalindrome = (N) => {
 
-    let largestPalindrome = 0;
-
     // iterate through range of multiplicands
     for (let i = 10 ** (N - 1); i < 10 ** N; i++) {
-        for (let j = 10 ** (N - 1); j < 10 ** N; j++) {
-
-            // check if the product is a palindrome
-            if (isPalindrome(i * j)) {
-
-                // check if it's larger than our largest palindrome
-                if (i * j > largestPalindrome) {
-                    
-                    // update largest palindrome
-                    largestPalindrome = i * j;
-                }
-            }
+        num = i**2
+        // check if the product is a palindrome
+        if (isPalindrome(num)) {
+            return num
+        }
+        num = i * (i-1)
+        // check if the product is a palindrome
+        if (isPalindrome(num)) {
+            return num
         }
     }
 
     // return result
-    return largestPalidrome;
+    return 0;
 }
 
 // input
-const N = 3;
+const N = 7;
 
 // CHALLENGE: Can you optimize the algorithm above so that works for input values N=4, N=5, N=6 
 //  in a "reasonable" amount of time?
-
+for (let j = 5; j > 0; j--){
 // execution
 console.time(exports.name)
 exports.getLargestPalindrome(N)
 console.timeEnd(exports.name)
+}
